0655b0029705e42d26e0ba1306020eb50c276328
Fbortone <fbortone@netplus.ch>
2024-10-29 23:46:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans l'application de suivi des heures</h1>
+    </div>
+  );
+}
+
+export default App;
